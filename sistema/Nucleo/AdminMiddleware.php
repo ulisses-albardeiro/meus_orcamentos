88d8eed690b1ef3b84bfe59f8187b46541361fad
Ulisses Albardeiro <ualbardeiro@gmail.com>
2025-09-02 19:08:59 -0300

    Ajuste no midleware de admin

--- sistema/Nucleo/AdminMiddleware.php
+++ sistema/Nucleo/AdminMiddleware.php
@@ -4,24 +4,21 @@ namespace sistema\Nucleo;
 
 use Pecee\Http\Middleware\IMiddleware;
 use Pecee\Http\Request;
-use sistema\Controlador\UsuarioControlador;
 use sistema\Modelos\UsuarioModelo;
 use sistema\Nucleo\Helpers;
 use sistema\Nucleo\Mensagem;
 
 class AdminMiddleware implements IMiddleware
 {
-    protected $usuario;
     protected Mensagem $mensagem;
-    protected Sessao $sessao;
 
     public function handle(Request $request): void
     {
-        $this->sessao = (new Sessao)->usuarioId;
+        $usuario = (new UsuarioModelo)->buscaPorId((new Sessao)->usuarioId);
 
-        if ($this->sessao->nivel != 1) {
+        if ($usuario->nivel != 1) {
             $mensagem = (new Mensagem());
-            $mensagem->mensagemErro('Acesso negado.')->flash();
+            $mensagem->mensagemErro('Acesso negado!')->flash();
             Helpers::redirecionar("login");
             exit;
         }
