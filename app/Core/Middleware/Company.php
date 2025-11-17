80e1f602c7b8806ce8b13f240a4860af603cadb9
Ulisses Albardeiro <ualbardeiro@gmail.com>
2025-11-17 18:41:23 -0300

    refactor: refactor Company middleware: simplify logic and update redirect route

--- app/Core/Middleware/Company.php
+++ app/Core/Middleware/Company.php
@@ -3,25 +3,17 @@
 namespace App\Core\Middleware;
 
 use Pecee\Http\Middleware\IMiddleware;
-use Pecee\Http\Request;
 use App\Models\CompanyModel;
+use Pecee\Http\Request;
 use App\Core\Helpers;
-use App\Core\Message;
 use App\Core\Session;
 
 class Company implements IMiddleware
 {
-    protected Message $mensagem;
-    protected CompanyModel $empresaModelo;
-    protected Session $sessao;
-
     public function handle(Request $request): void
     {
-        $this->mensagem = new Message();
-        $this->sessao = new Session();
-        $this->empresaModelo = new CompanyModel();
-        $empresa = $this->empresaModelo->findCompanyByUserId($this->sessao->userId);
-        if ($empresa === null) {
+        $company = (new CompanyModel)->findCompanyByUserId((new Session)->userId);
+        if (empty($company)) {
             Helpers::redirect('company/create');
         }
     }
